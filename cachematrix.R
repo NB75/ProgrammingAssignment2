b9caf43c87dd3b70b9c7273247f8881a26ab1d01
NB <nicobron@hotmail.it>
2015-03-17 23:36:18 +0100

    changes to cachematrix file for ProgAss2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions:
+## First function to cache matrix to invert and the inverted matrix
+## Second function that calculates the inverted matrix. If available in the cache, matrix won't be calculated
 
-## Write a short comment describing this function
+## Function to cache the content of a matrix and the correspondent inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- matrix()
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Function that tries to read value of inverted matrix from cache, if available
+## If matrix is not available in cache, calculation is performed online
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
